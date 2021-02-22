1893c2413356f6c9334caa0793ce79cf260a0fd6
Shan Kai <48236492+shankaikai@users.noreply.github.com>
2021-02-22 22:00:54 +0800

    added edit and delete functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,102 +1,174 @@
-import './App.css';
-import { useState } from 'react';
-import Axios from 'axios'
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
-
   // States
   const [name, setName] = useState("");
   const [age, setAge] = useState(0);
   const [country, setCountry] = useState("");
   const [position, setPosition] = useState("");
   const [wage, setWage] = useState(0);
-
-  const [employeeList, setEmployeeList] = useState([])
+  const [newWage, setNewWage] = useState(0);
+  const [employeeList, setEmployeeList] = useState([]);
 
   // Use Axios lib to send requests to an API
   const addEmployee = () => {
     // POST to the /create end point
     // endpoint, body
     Axios.post("http://localhost:3001/create", {
-      name: name, 
-      age: age, 
-      country: country, 
-      position: position, 
-      wage: wage
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
     }).then(() => {
-      // then is a promise that calls a callback function 
+      // then is a promise that calls a callback function
 
       // Clear the inputs
-      setName("")
-      setAge(0)
-      setCountry("")
-      setPosition("")
-      setWage(0)
+      setName("");
+      setAge(0);
+      setCountry("");
+      setPosition("");
+      setWage(0);
 
       // Add it straight to the employeeList
-      setEmployeeList([...employeeList, {
-        name: name, 
-        age: age, 
-        country: country, 
-        position: position, 
-        wage: wage
-      }])
-    })
-  }
+      setEmployeeList([
+        ...employeeList,
+        {
+          name: name,
+          age: age,
+          country: country,
+          position: position,
+          wage: wage,
+        },
+      ]);
+    });
+  };
 
   // Axios lib GET req
   const getEmployees = () => {
     Axios.get("http://localhost:3001/employee").then((response) => {
       // set the Employee List state var
-      setEmployeeList(response.data)
-    })
-  }
+      setEmployeeList(response.data);
+    });
+  };
 
-  // debug function
-  // const displayinfo = () => {
-  //   console.log(name + age + country + position + wage)
-  // }
+  const deleteEmployee = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
+      setEmployeeList(
+        employeeList.filter((val) => {
+          // going through every element in the list and if the condition is met then return
+          return val.id != id;
+        })
+      );
+    });
+  };
+
+  // PUT req need to pass an object also
+  // Pass id from val.id from database
+  const updateEmployeeWage = (id) => {
+    Axios.put("http://localhost:3001/update", {
+      wage: newWage,
+      id: id,
+    }).then((response) => {
+      // Change the wage of the id that was changed
+      setEmployeeList(
+        employeeList.map((val) => {
+          return val.id === id
+            ? {
+                id: val.id,
+                name: val.name,
+                country: val.country,
+                age: val.age,
+                position: val.position,
+                wage: newWage,
+              }
+            : val;
+        })
+      );
+    });
+  };
 
   return (
     <div className="App">
       <div className="information">
         <label>Name:</label>
-        <input type ="text" onChange = {(event) => {
-          setName(event.target.value)
-        }}></input>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        ></input>
         <label>Age:</label>
-        <input type = "number" onChange = {(event) => {
-          setAge(event.target.value)
-        }}></input>
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        ></input>
         <label>Country:</label>
-        <input type = "text" onChange = {(event) => {
-          setCountry(event.target.value)
-        }}></input>
+        <input
+          type="text"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        ></input>
         <label>Position:</label>
-        <input type = "text" onChange = {(event) => {
-          setPosition(event.target.value)
-        }}></input>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        ></input>
         <label>Wage (year):</label>
-        <input type = "number" onChange = {(event) => {
-          setWage(event.target.value)
-        }}></input>
-        <button onClick = {addEmployee}>Add Employee</button>
+        <input
+          type="number"
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        ></input>
+        <button onClick={addEmployee}>Add Employee</button>
       </div>
-      <div className = "divider"></div>
-      <div className = "employees">
-        <button onClick = {getEmployees}>Show Employees</button>
+      <div className="divider"></div>
+      <div className="employees">
+        <button onClick={getEmployees}>Show Employees</button>
         {/* mapping through all the entries in the list */}
         {employeeList.map((val, key) => {
           return (
-            <div className = "employeeDetails">
-              <h2>Employee</h2>
-              <h3>Name: {val.name}</h3> 
-              <h3>Age: {val.age}</h3> 
-              <h3>Country: {val.country}</h3> 
-              <h3>Postion: {val.position}</h3> 
-              <h3>Wage: {val.wage}</h3> 
+            <div className="employeeDetails">
+              <div>
+                <h3>Name: {val.name}</h3>
+                <h3>Age: {val.age}</h3>
+                <h3>Country: {val.country}</h3>
+                <h3>Postion: {val.position}</h3>
+                <h3>Wage: {val.wage}</h3>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="New Wage"
+                  onChange={(event) => {
+                    setNewWage(event.target.value);
+                  }}
+                ></input>
+                <button
+                  onClick={() => {
+                    updateEmployeeWage(val.id);
+                  }}
+                >
+                  Update
+                </button>
+                <button
+                  onClick={() => {
+                    deleteEmployee(val.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
             </div>
-          )
+          );
         })}
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,61 +1,93 @@
 // Create instance of express lib
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 //Import mysql
-const mysql = require("mysql")
+const mysql = require("mysql");
 
 // Cors
-const cors = require("cors")
-app.use(cors())
-app.use(express.json()) //json parser
+const cors = require("cors");
+app.use(cors());
+app.use(express.json()); //json parser
 
 // Database connection used for all functions
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: 'shankai123',
-    database: 'employeeSystem'
-})
+  user: "root",
+  host: "localhost",
+  password: "shankai123",
+  database: "employeeSystem",
+});
 
 // POST req - create route called create
-app.post('/create', (req, res) => {
-    console.log(req.body)
-    // Get variables from front end
-    const name = req.body.name //req.body.[variable name]
-    const age = req.body.age
-    const country = req.body.country
-    const position = req.body.position
-    const wage = req.body.wage
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  // Get variables from front end
+  const name = req.body.name; //req.body.[variable name]
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
 
-    // insert sql statement
-    // each variable inthe array in the 2nd arg will replace the ?
-    // 3rd arg is a callback function to call once query is done
-    db.query('INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)', 
-    [name, age, country, position, wage], 
+  // insert sql statement
+  // each variable inthe array in the 2nd arg will replace the ?
+  // 3rd arg is a callback function to call once query is done
+  db.query(
+    "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
     (err, result) => {
-        if (err) {
-            console.log(err)
-        } else {
-            // Put the message into the result of the post
-            res.send("Values inserted")
-        }
-    }) 
-}) 
+      if (err) {
+        console.log(err);
+      } else {
+        // Put the message into the result of the post
+        res.send("Values inserted");
+      }
+    }
+  );
+});
 
 // GET req at /employee endpoint
-app.get('/employee', (req, res) => {
-    db.query("SELECT * FROM employees", (err, result) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.send(result)
-        }
-    })
-})
+app.get("/employee", (req, res) => {
+  db.query("SELECT * FROM employees", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
 
+// PUT req at /update
+app.put("/update", (req, res) => {
+  const id = req.body.id;
+  const wage = req.body.wage;
+  db.query(
+    "UPDATE employees SET wage = ? WHERE id = ?",
+    [wage, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+        console.log("Update success");
+      }
+    }
+  );
+});
+
+// DELETE request
+app.delete("/delete/:id", (req, res) => {
+  // : means params
+  const id = req.params.id;
+  db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+      console.log("Delete success");
+    }
+  });
+});
 
 app.listen(3001, () => {
-    console.log("Yay the server is running yo on port 3001")
-})
-
+  console.log("Yay the server is running yo on port 3001");
+});
